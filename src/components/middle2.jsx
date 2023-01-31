6b7ef1bf088a97a7459c66d185e7734e6b146088
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-01-31 12:37:59 +0530

    Update middle2.jsx
    kljbhvgf

--- src/components/middle2.jsx
+++ src/components/middle2.jsx
@@ -5,11 +5,11 @@ function middle2(){return(<div>
 
 
 <h1 className="M2h1" style={{"
-    left: 50px;
-    right: 0px;
-    margin-top: -500;
-    top: -55;
-    margin-bottom: 200px;"}}
+    left: 50px,
+    right: 0px,
+    margin-top: -500,
+    top: -55,
+    margin-bottom: 200px"}}
                           >My Services</h1>
 <div class="shell">
   <div class="containerr">
