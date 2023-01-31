55cac8562aa7f34233572252f90b488a558354cd
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-01-31 11:55:20 +0530

    Update middle2.jsx

--- src/components/middle2.jsx
+++ src/components/middle2.jsx
@@ -1,137 +1,216 @@
 import React from "react";
 
-function middle2(){return(<body style={{backgroundColor:"#333"}}>
+function middle2(){return(<div>
+
+
+
 <h1 className="M2h1" >My Services</h1>
-    <div class="container first">
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                <svg xmlns="http://www.w3.org/2000/svg" width="86" height="86" fill="white" class="bi bi-motherboard" viewBox="0 0 16 16">
+<div class="shell">
+  <div class="containerr">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="wsk-cp-product">
+          <div class="wsk-cp-img">
+          <svg xmlns="http://www.w3.org/2000/svg" width="246" height="156" fill="black" class="bi bi-motherboard" viewBox="0 0 16 16">
   <path d="M11.5 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5Zm2 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5Zm-10 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6Zm0 2a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6ZM5 3a1 1 0 0 0-1 1h-.5a.5.5 0 0 0 0 1H4v1h-.5a.5.5 0 0 0 0 1H4a1 1 0 0 0 1 1v.5a.5.5 0 0 0 1 0V8h1v.5a.5.5 0 0 0 1 0V8a1 1 0 0 0 1-1h.5a.5.5 0 0 0 0-1H9V5h.5a.5.5 0 0 0 0-1H9a1 1 0 0 0-1-1v-.5a.5.5 0 0 0-1 0V3H6v-.5a.5.5 0 0 0-1 0V3Zm0 1h3v3H5V4Zm6.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2Z"/>
   <path d="M1 2a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-2H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 9H1V8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6H1V5H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 2H1Zm1 11a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v11Z"/>
 </svg>
-                    {/* <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"></img> */}
-                    <h3>Back-End Development</h3>
-                </div>
+            {/* <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true" alt="Product" class="img-responsive" /> */}
+          </div>
+          <div class="wsk-cp-text">
+            <div class="category">
+              <span>Back-End Development</span>
             </div>
-            <div class="face face2">
-                <div class="content">
-                    <p>It is the server-side of web development, which involves the creation and management of the underlying systems and technologies that power a website or application.It includes the development of databases, servers, and APIs.</p>
-                </div>
+            <div class="description-prod">
+              <p>It is the server-side of web development, which involves the creation and management of the underlying systems and technologies that power a website or application.It includes the development of databases, servers, and APIs.</p>
             </div>
+          </div>
         </div>
-        <hr></hr>
-<hr></hr>
-        {/* <div class="card">
-            <div class="face face1">
-                <div class="content">
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"></img>
-                    <h3>Design</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        <a href="#">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"></img>
-                    <h3>Code</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        <a href="#">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"></img>
-                    <h3>Launch</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        <a href="#">Read More</a>
-                </div>
-            </div>
-        </div> */}
-    </div>
-
-
-
-    <div class="container second">
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                <svg xmlns="http://www.w3.org/2000/svg" width="86" height="86" fill="white" class="bi bi-pc-display-horizontal" viewBox="0 0 16 16">
-  <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5v7A1.5 1.5 0 0 0 1.5 10H6v1H1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5v-1h4.5A1.5 1.5 0 0 0 16 8.5v-7A1.5 1.5 0 0 0 14.5 0h-13Zm0 1h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5ZM12 12.5a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0Zm2 0a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0ZM1.5 12h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1ZM1 14.25a.25.25 0 0 1 .25-.25h5.5a.25.25 0 1 1 0 .5h-5.5a.25.25 0 0 1-.25-.25Z"/>
-</svg>
-                    {/* <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"></img> */}
-                    <h3>Front-End Development</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                    <p>Involves the design and development of the user interface and experience of a website or application. It includes the use of HTML, CSS, and JavaScript to create the visual elements for website or application. </p>
-                </div>
-            </div>
-        </div>
-        </div>
-
-
-
-        <div class="container third" >
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                <svg xmlns="http://www.w3.org/2000/svg" width="86" height="86" fill="white" class="bi bi-shield-lock-fill" viewBox="0 0 16 16">
+      </div>
+      <div class="col-md-4">
+        <div class="wsk-cp-product">
+          <div class="wsk-cp-img">
+          <svg xmlns="http://www.w3.org/2000/svg" width="246" height="156" fill="black" class="bi bi-shield-lock-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z"/>
 </svg>
-                    {/* <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"></img> */}
-                    <h3>Web Application Security</h3>
-                </div>
+       </div>
+          {/* <img src="https://1.bp.blogspot.com/-b-2SgNUrFHg/WVSvmewWqgI/AAAAAAAAAK0/1K4YCcbYjhokHwV_IgiVJN9mEnQoWunIwCPcBGAYYCw/s1600/fashion-portrait-nicole-6347.jpg" alt="Product" class="img-responsive" /></div> */}
+          <div class="wsk-cp-text">
+            <div class="category">
+              <span>Web Security</span>
             </div>
-            <div class="face face2">
-                <div class="content">
-                    <p>It takes 20 years to build a reputation and few minutes of cyber-incident to ruin it. Let me test your Web Application. API Penetration testing involves all processes of checking for vulnerabilities and building strong endpoints in your APIs.</p>
-                </div>
+            <div class="description-prod">
+              <p>It takes 20 years to build a reputation and few minutes of cyber-incident to ruin it. Let me test your Web Application. API Penetration testing involves all processes of checking for vulnerabilities and building strong endpoints in your APIs.</p>
             </div>
+          </div>
         </div>
-        </div>
-
-
-
-        <div class="container forth">
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                {/* <svg xmlns="http://www.w3.org/2000/svg" width="86" height="86" fill="white" class="bi bi-person-workspace" viewBox="0 0 16 16">
-  <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-  <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z"/>
-           </svg> */}
-                    <img src="/imeges/image.jpg"></img>
-                    <h3>Social Engineering</h3>
-                </div>
+      </div>
+      <div class="col-md-4">
+        <div class="wsk-cp-product">
+          <div class="wsk-cp-img">
+          <svg xmlns="http://www.w3.org/2000/svg" width="246" height="156" fill="black" class="bi bi-pc-display-horizontal" viewBox="0 0 16 16">
+  <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5v7A1.5 1.5 0 0 0 1.5 10H6v1H1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5v-1h4.5A1.5 1.5 0 0 0 16 8.5v-7A1.5 1.5 0 0 0 14.5 0h-13Zm0 1h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5ZM12 12.5a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0Zm2 0a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0ZM1.5 12h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1ZM1 14.25a.25.25 0 0 1 .25-.25h5.5a.25.25 0 1 1 0 .5h-5.5a.25.25 0 0 1-.25-.25Z"/>
+</svg>
+          {/* <img src="https://3.bp.blogspot.com/-iUes8qr4uC8/WVSvl6Wf_fI/AAAAAAAAAKs/JiTUUVvdbqEC_QGGaQhuVJiezIN3LIqEgCPcBGAYYCw/s1600/IMG_5367-bp.jpg" alt="Product" class="img-responsive" /> */}
+          </div>
+          <div class="wsk-cp-text">
+            <div class="category">
+              <span>Front-End Development</span>
             </div>
-            <div class="face face2">
-                <div class="content">
-                    <p>It is the use of psychological manipulation to trick individuals into divulging sensitive information or performing actions. Social engineering training is designed to teach individuals how to recognize and defend against these tactics.</p>
-                </div>
+            <div class="description-prod">
+              <p>Involves the design and development of the user interface and experience of a website or application. It includes the use of HTML, CSS, and JavaScript to create the visual elements for website or application.</p>
             </div>
+          </div>
         </div>
-        
+      </div>
+    </div>
+  </div>
 </div>
-    
-</body>
+
+<div class="shell1">
+  <div class="containerr">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="wsk-cp-product">
+          <div class="wsk-cp-img">
+          <img src="/imeges/image.jpg"></img>
+            {/* <img src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg" alt="Product" class="img-responsive" /> */}
+          </div>
+          <div class="wsk-cp-text">
+            <div class="category">
+              <span>Social Engineering</span>
+            </div>
+            <div class="description-prod">
+              <p>It is the use of psychological manipulation to trick individuals into divulging sensitive information or performing actions. Social engineering training is designed to teach individuals how to recognize and defend against these tactics.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="wsk-cp-product">
+          <div class="wsk-cp-img">
+          <svg xmlns="http://www.w3.org/2000/svg" width="246" height="156" fill="currentColor" class="bi bi-terminal" viewBox="0 0 16 16">
+  <path d="M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9zM3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708l-2-2z"/>
+  <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12z"/>
+</svg>
+          {/* <img src="https://1.bp.blogspot.com/-b-2SgNUrFHg/WVSvmewWqgI/AAAAAAAAAK0/1K4YCcbYjhokHwV_IgiVJN9mEnQoWunIwCPcBGAYYCw/s1600/fashion-portrait-nicole-6347.jpg" alt="Product" class="img-responsive" /> */}
+          </div>
+          <div class="wsk-cp-text">
+            <div class="category">
+              <span>C & C++</span>
+            </div>
+            <div class="description-prod">
+              <p>C is used in system programming and game development. C++ as an extension of C with object-oriented features - is used for developing a wide range of applications, from desktop to web and mobile..</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="wsk-cp-product">
+          <div class="wsk-cp-img">
+          
+          <img src="/imeges/1174949_js_react js_logo_react_react native_icon (1).png" alt="Product" class="img-responsive" />
+          </div>
+          <div class="wsk-cp-text">
+            <div class="category">
+              <span>React</span>
+            </div>
+            <div class="description-prod">
+              <p>React is a JavaScript library for building user interfaces. It was developed and is maintained by Facebook and is used for building single-page applications and mobile applications.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+    {/* <div class="container">
+    <div class="cards grid-row">
+      <div class="card">
+        <div class="card-top">
+          <img src="img1.jpg" alt="Blog Name"></img>
+        </div>
+        <div class="card-info">
+          <h2>JavaScript Quote Generator</h2>
+          <span class="date">Monday, Jan 20, 2021</span>
+          <p class="excerpt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+        <div class="card-bottom flex-row">
+          <a href="#" class="read-more">Read Full Blog</a>
+          <a href="#" class="button btn-yellow">Blog</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-top">
+          <img src="img2.jpg" alt="Blog Name"></img>
+        </div>
+        <div class="card-info">
+          <h2>How to Build HTML Minifier</h2>
+          <span class="date">Monday, Jan 20, 2021</span>
+          <p class="excerpt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+        <div class="card-bottom flex-row">
+          <a href="#" class="read-more">Read Full Blog</a>
+          <a href="#" class="button btn-sky">Blog</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-top">
+          <img src="img3.jpg" alt="Blog Name"></img>
+        </div>
+        <div class="card-info">
+          <h2>How to Build Signature Pad</h2>
+          <span class="date">Monday, Jan 20, 2021</span>
+          <p class="excerpt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+        <div class="card-bottom flex-row">
+          <a href="#" class="read-more">Read Full Blog</a>
+          <a href="#" class="button btn-dpink">Blog</a>
+        </div>
+      </div>
+    </div>    
+  </div>
+
+  <div class="container">
+    <div class="cards grid-row">
+      <div class="card">
+        <div class="card-top">
+          <img src="img1.jpg" alt="Blog Name"></img>
+        </div>
+        <div class="card-info">
+          <h2>JavaScript Quote Generator</h2>
+          <span class="date">Monday, Jan 20, 2021</span>
+          <p class="excerpt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+        <div class="card-bottom flex-row">
+          <a href="#" class="read-more">Read Full Blog</a>
+          <a href="#" class="button btn-yellow">Blog</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-top">
+          <img src="img4.jpeg" alt="Blog Name"></img>
+        </div>
+        <div class="card-info">
+          <h2>How to Build Gsap Gallery</h2>
+          <span class="date">Monday, Jan 20, 2021</span>
+          <p class="excerpt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+        <div class="card-bottom flex-row">
+          <a href="#" class="read-more">Read Full Blog</a>
+          <a href="#" class="button btn-dpink">Blog</a>
+        </div>
+      </div>
+    </div>    
+  </div> */}
+
+  
 
 
 
@@ -140,6 +219,8 @@ function middle2(){return(<body style={{backgroundColor:"#333"}}>
 
 
 
+
+</div>
 )
 
 
