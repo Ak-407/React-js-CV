d391b82dd533cc6e9cc48c68917427786cf59039
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-02-01 13:38:43 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function footer(){return(<dev className="FEND">
 
 <div className="FEND1 container" style={{width: "615px"}}>
-<span><h1><a style={{color:"white"}} href="https://www.linkedin.com/in/syed-amaan-88b250226/">linkedin</a></h1></span>
-<span><h1><a style={{color:"white"}} href="https://www.instagram.com/amaan_3k/">Instagram</a></h1></span>
-<span><h1><a style={{color:"white"}} href="https://github.com/Ak-407">Github</a></h1></span>
+<span><h1><a style={{color:"white"}} href="https://www.linkedin.com/in/syed-amaan-88b250226/">Github</a></h1></span>
+<span><h1><a style={{color:"white"}} href="https://www.instagram.com/amaan_3k/">linkedin</a></h1></span>
+<span><h1><a style={{color:"white"}} href="https://github.com/Ak-407">Instagram</a></h1></span>
 
 </div>
 <div className="FEND2 container" style={{width: "615px"}}>
