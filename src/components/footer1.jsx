f188f086ae9fd5429f9138223b083ffe17f996a6
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-02-01 12:08:20 +0530

    Update footer1.jsx
    neww

--- src/components/footer1.jsx
+++ src/components/footer1.jsx
@@ -6,10 +6,17 @@ function footer1(){
     return(<dev>
 
     <dev className="F1">
+           <hr style={{color:"white"}}></hr>
+    <hr style={{color:"white"}}></hr>
+    <hr style={{color:"white"}}></hr>
+    <hr style={{color:"white"}}></hr>
     <p>Your developing partner ;)</p> 
 
     <h1>I offer freelance services on a project basis, providing exceptional and professional solutions to meet client needs.</h1>
-
+    <hr style={{color:"white"}}></hr>
+    <hr style={{color:"white"}}></hr>
+    <hr style={{color:"white"}}></hr>
+    <hr style={{color:"white"}}></hr>
     </dev>
     <dev className="F2" class="wrap">
     <button class="buttonF">Lets Talk</button>
