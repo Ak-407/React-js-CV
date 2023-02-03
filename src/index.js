ca66d2fc2cea32210187776a241876673c84c140
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-02-03 16:45:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import First from './components/first';
+import Head from './components/head';
+import Middle from './components/middle';
+import Middle2 from './components/middle2';
+import Middle3 from './components/middle3';
+import Middle4 from './components/middle4';
+import Footer1 from './components/footer1';
+import Footer from './components/footer';
+import PageReloader from './components/new';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+ReactDOM.render(
+
+<div>
+
+<PageReloader  />
+<header>
+<First />
+<Head />
+<Middle />
+<Middle3 />
+<Middle2 />
+<Middle4 />
+<Footer1 />
+<Footer />
+</header>
+
+
+ 
+
+</div>
+, document.getElementById("root"));
