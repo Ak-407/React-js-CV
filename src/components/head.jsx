7acd96ee7d005f754b1054c1d83fe39298d1ccb4
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-02-03 16:49:27 +0530

    Update head.jsx

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,38 +1,19 @@
 import React from "react";
-import $ from "jquery";
 
 
 function head(){
+ 
   
-  const section = document.querySelector("section");
-let currentPixel = window.pageYOffset
-  const looper = function () {
-    const newPixel = window.pageYOffset;
-    const diff = newPixel - currentPixel
-    const speed = diff * 0.35;
-    
-    section.style.transform = "skewY(" + speed + "deg)"
-    
-    
-    currentPixel = newPixel;
-    
-    requestAnimationFrame(looper)
-  }
-  
-  
-    return(<div id="HHH" className='scroll-wrapper '>
+    return(<div className="scroll-wrapper">
     <div className="scroll-wrapper2">
-    <span id="PHead" >
+    <span id="PHead">
         {/* <img id="PHead" src="/imeges/P_20221211_154157-05.jpeg" alt="amaan"></img> */}
         {window.addEventListener('scroll', function(){
          this.document.getElementById("PHead").style.backgroundSize =200 -  +window.pageYOffset/60+"%";
          this.document.getElementById("PHead").style.opacity =95 -  +window.pageYOffset/100+"%";
-         this.looper();
      })}
 </span>
 <span>
-{window.addEventListener('scroll', function(){
-this.looper();})}
         <main class="Hcontainer">
   <p>Hello 👋 I'm</p>
   <section class="animation">
@@ -46,7 +27,7 @@ this.looper();})}
         <hr></hr>
         Am a Full Stack Engineer.
         </h1> */}
-</span>
+    </span>
     </div>
     </div>
     )
